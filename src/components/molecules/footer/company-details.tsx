2a4942c8eef62cdb7a0c993d37194900eb340f64
knocknockguesswho <nullengineer@gmail.com>
2021-09-20 12:22:49 +0700

    fix content

--- src/components/molecules/footer/company-details.tsx
+++ src/components/molecules/footer/company-details.tsx
@@ -12,19 +12,10 @@ const CompanyDetails = (props: IProps) => {
   return (
     <div className='flex flex-col space-y-3'>
       <div className='flex flex-col items-baseline space-y-3'>
-        <Image
-          src={getStatic('icons/isa-logo-text.svg')}
-          width={200}
-          height={65}
-          alt='isa-logo-text'
-          loading='lazy'
-        />
+        <Image src={getStatic('icons/isa-logo-text.svg')} width={200} height={65} alt='isa-logo-text' loading='lazy' />
         <Typography className='text-sm phone:text-s'>{props.address}</Typography>
       </div>
-      <Typography
-        italic
-        className='text-s'
-      >
+      <Typography italic className='text-s'>
         {props.email}
       </Typography>
     </div>
@@ -32,11 +23,25 @@ const CompanyDetails = (props: IProps) => {
 };
 
 CompanyDetails.defaultProps = {
-  address: <>
-    Gedung Epiwalk Lt.5, Unit B547-548,<br/>Jln. H. R. Rasuna Said Rt.2/Rw/5, Karet Kuningan, Jakarta Selatan,<br/>Daerah Khusus Ibukota Jakarta 12940.<br/><br/>
-    <i>Crosscoop Co-workingspace</i><br/>Centennial Tower Lt. 35<br/>l. Gatot Subroto, RT.2/RW.2, Karet Semanggi, Setiabudi, Jakarta Selatan,<br/>Daerah Khusus Ibukota Jakarta 12930
-  </>,
-  email: 'Email: info@iradhi.com'
+  address: (
+    <>
+      Gedung Epiwalk Lt.5, Unit B547-548,
+      <br />
+      Jl. H. R. Rasuna Said Rt.2/Rw/5, Karet Kuningan, Jakarta Selatan,
+      <br />
+      Daerah Khusus Ibukota Jakarta 12940.
+      <br />
+      <br />
+      <i>Crosscoop Co-workingspace</i>
+      <br />
+      Centennial Tower Lt. 35
+      <br />
+      l. Gatot Subroto, RT.2/RW.2, Karet Semanggi, Setiabudi, Jakarta Selatan,
+      <br />
+      Daerah Khusus Ibukota Jakarta 12930
+    </>
+  ),
+  email: 'Email: info@iradhi.com',
 };
 
 export default CompanyDetails;
--- src/components/molecules/footer/footer-copyright-section.tsx
+++ src/components/molecules/footer/footer-copyright-section.tsx
@@ -4,11 +4,10 @@ import React from 'react';
 const FooterCopyrightSection = () => {
   return (
     <div className='w-full py-6 flex justify-center items-center bg-blue'>
-      <Typography
-        className='text-s tablet:text-xs phone:text-xs text-white'
-        center
-      >
-      © 2020 PT Iradhi Solusindo Abadi, All Rights Reserved.<br/>All trademarks are the property of their respective owners.
+      <Typography className='text-s tablet:text-xs phone:text-xs text-white' center>
+        © 2021 PT Iradhi Solusindo Abadi, All Rights Reserved.
+        <br />
+        All trademarks are the property of their respective owners.
       </Typography>
     </div>
   );
